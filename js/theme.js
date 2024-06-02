3161022b96aeb87f4917d05e622b22b2ca3c30b9
neMarshall <76066671+neMarshall@users.noreply.github.com>
2024-06-02 04:39:50 +0000

    prettier

--- js/theme.js
+++ js/theme.js
@@ -1,18 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const themeToggle = document.getElementById('theme-toggle');
-    const body = document.body;
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggle = document.getElementById("theme-toggle");
+  const body = document.body;
 
-    // Проверяем и устанавливаем текущую тему из локального хранилища
-    const currentTheme = localStorage.getItem('theme') || 'light-theme';
-    body.classList.add(currentTheme);
+  // Проверяем и устанавливаем текущую тему из локального хранилища
+  const currentTheme = localStorage.getItem("theme") || "light-theme";
+  body.classList.add(currentTheme);
 
-    // Обработчик события для переключения темы
-    themeToggle.addEventListener('click', () => {
-        body.classList.toggle('light-theme');
-        body.classList.toggle('dark-theme');
+  // Обработчик события для переключения темы
+  themeToggle.addEventListener("click", () => {
+    body.classList.toggle("light-theme");
+    body.classList.toggle("dark-theme");
 
-        // Сохраняем текущую тему в локальное хранилище
-        const newTheme = body.classList.contains('dark-theme') ? 'dark-theme' : 'light-theme';
-        localStorage.setItem('theme', newTheme);
-    });
+    // Сохраняем текущую тему в локальное хранилище
+    const newTheme = body.classList.contains("dark-theme")
+      ? "dark-theme"
+      : "light-theme";
+    localStorage.setItem("theme", newTheme);
+  });
 });
