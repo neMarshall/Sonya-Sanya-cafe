719c30e020af405cc49cecb5995a48354428c4dc
neMarshall <76066671+neMarshall@users.noreply.github.com>
2024-06-02 04:38:06 +0000

    themes

---
+++ js/theme.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    // Проверяем и устанавливаем текущую тему из локального хранилища
+    const currentTheme = localStorage.getItem('theme') || 'light-theme';
+    body.classList.add(currentTheme);
+
+    // Обработчик события для переключения темы
+    themeToggle.addEventListener('click', () => {
+        body.classList.toggle('light-theme');
+        body.classList.toggle('dark-theme');
+
+        // Сохраняем текущую тему в локальное хранилище
+        const newTheme = body.classList.contains('dark-theme') ? 'dark-theme' : 'light-theme';
+        localStorage.setItem('theme', newTheme);
+    });
+});
